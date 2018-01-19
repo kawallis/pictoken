97ad3b6ced22a5e3f38624f2a5a4a3f5a404a760
Chris_Wallace <kawallis@icloud.com>
2018-01-19 10:32:46 -0800

    init

---
+++ src/About.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
+import { Row, Col, Button, Icon } from 'antd';
+
+export default class About extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <Row style={{marginTop: '15vh', marginBottom: '15vh'}}>
+                    <Col span={12} style={{display: 'flex', justifyContent: 'center'}}>
+                        <img src={require('./static/girlSmile.jpg')} style={{
+                            height: '80vh',
+                        }} />
+                    </Col>
+
+                    <Col span={12} style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                            <h1 style={{color: 'aqua'}}>About PictoToken</h1>
+                            <h2 style={{color: 'black', fontSize: '3em'}}>We Are Reinventing How Photographers Share  Their Memories</h2>
+                            <p style={{}}>A decenteralized platform for images thats super cool and promises to do amazing things for everyone, and everywhere A decenteralized platform for images thats super cool and promises to do amazing things for everyone, and everywhere A decenteralized platform for images thats super cool and promises to do amazing things for everyone, and everywhere</p>
+                            <Button type="primary" style={{width: '15vw'}}>
+                                See How<Icon type="right" />
+                            </Button>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
+}
---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
+import ScrollableAnchor from 'react-scrollable-anchor'
+import About from './About';
+import Badges from './Badges';
+
+import { Menu, Icon, Carousel } from 'antd';
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
+
+export default class App extends React.Component {
+  state = {
+    current: 'mail',
+  }
+  handleClick = (e) => {
+    this.setState({
+      current: e.key,
+    });
+  }
+  render() {
+    let images = ['picOne.jpg', 'picTwo.jpg', 'picThree.jpg', 'picFour.jpg'];
+    return (
+      <div>
+        <Menu
+          onClick={this.handleClick}
+          selectedKeys={[this.state.current]}
+          mode="horizontal"
+        >
+          <Menu.Item key="about">
+            <Icon type="home" />Picto
+          </Menu.Item>
+          <Menu.Item key="about">
+            <Icon type="idcard" />About
+          </Menu.Item>
+          <Menu.Item key="product">
+            <Icon type="camera" />Product
+          </Menu.Item>
+          <Menu.Item key="whitepaper">
+            <Icon type="profile" />White Paper
+          </Menu.Item>
+          <Menu.Item key="road">
+            <Icon type="compass" />Road Map
+          </Menu.Item>
+          <Menu.Item key="tokens">
+            <Icon type="pie-chart" />Token Distribution
+          </Menu.Item>
+          <Menu.Item key="team">
+            <Icon type="team" />Team
+          </Menu.Item>
+        </Menu>
+        <div style={{zIndex: 20, position: 'absolute', left: 0, right: 0, textAlign: 'center', top: '30vh'}}>
+            <h1 style={{color: 'white', fontSize: '8em'}}>PictoToken</h1>
+            <h3 style={{color: 'white'}}>A decenteralized platform for images</h3>
+            <Icon style={{color: 'white', fontSize: '4em', marginTop: '12vh'}} type="down-circle-o" />
+        </div>
+        <Carousel vertical autoplay={true}>
+          {images.map(image => {
+            return <img src={require(`./static/${image}`)} style={{
+              height: '95vh',
+              width: '100vw'
+            }} />
+          })}
+        </Carousel>
+        <About />
+        <Badges />
+      </div>
+    );
+  }
+}
